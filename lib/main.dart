6f0fe271ba3741e2dd69fc0a70e1f5a8f3c90b54
kanari3 <kanari333333@gmail.com>
2020-03-19 00:26:49 +0900

    add indicator

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,61 @@ class WebViewExample extends StatefulWidget {
 class _WebViewExampleState extends State<WebViewExample> {
   WebViewController _controller;
 
+  final _key = UniqueKey();
+
+  num _stackToView = 1;
+
+  void _handleLoad(String value) {
+    setState(() {
+      _stackToView = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Webview Demo'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add_to_home_screen),
-            onPressed: () {
-              _controller.loadUrl('https://www.google.co.jp');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.add_alert),
-            onPressed: () {
-              showDialog(context: context, builder: (context) {
-                return AlertDialog(title: Text('open dialog'),);
-              });
-            },
-          ),
-        ],
-      ),
-      body: WebView(
-        initialUrl: 'https://www.google.co.jp',
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController controller) {
-          _controller = controller;
-        },
-      ),
+        appBar: AppBar(
+          title: Text('Webview Demo'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add_to_home_screen),
+              onPressed: () {
+                _controller.loadUrl('https://www.google.co.jp');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.add_alert),
+              onPressed: () {
+                showDialog(context: context, builder: (context) {
+                  return AlertDialog(title: Text('open dialog'),);
+                });
+              },
+            ),
+          ],
+        ),
+        body: IndexedStack(
+          index: _stackToView,
+          children: [
+            Column(
+              children: < Widget > [
+                Expanded(
+                    child: WebView(
+                      key: _key,
+                      javascriptMode: JavascriptMode.unrestricted,
+                      initialUrl: "https://www.google.co.jp",
+                      onPageFinished: _handleLoad,
+                    )
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.white,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          ],
+        )
     );
   }
 }
